61575476a843210ef293b5b956c04d85e1d417bc
Damian Boytchev <damian.boytchev@gmail.com>
2023-11-02 21:23:57 +0200

    Add code from extra lesson.

---
+++ Week04Extra/main.cpp
@@ -0,0 +1,194 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <stack>
+
+bool isOperator(const char c)
+{
+    switch (c)
+    {
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+        return true;    
+    default:
+        return false;
+    }
+}
+
+std::string polishNotationConvert(const std::string& rpn)
+{
+    std::stack<std::string> s;
+    for (std::size_t i = 0; i < rpn.size(); i++)
+    {
+        if (!isOperator(rpn[i]))//bigbrain.begin();
+        {
+            s.push(std::string(1, rpn[i]));
+        } else
+        {
+            std::string result;
+            result+=rpn[i];
+            std::string op2 = s.top(); s.pop();
+            result+=s.top(); s.pop();
+            result+= op2;
+            s.push(result);
+        } //bigbrain.end();
+    }
+    std::string result;
+    while (!s.empty())
+    {
+        result+=s.top(); s.pop();
+    }
+    
+    return result;
+}
+
+const unsigned int labSize = 5;
+
+struct Point {
+    std::size_t row, col;
+    Point(std::size_t row, std::size_t col) : row(row), col(col) {}
+    
+    Point operator+ (const Point &other) const {
+        return Point(row + other.row, col + other.col);
+    }
+
+    bool operator==(const Point &other) const {
+        return row == other.row && col == other.col;
+    } 
+
+    bool checkBounds(std::size_t labSize = ::labSize) const {
+        return row < labSize && col < labSize;
+    }
+
+    friend std::ostream &operator<< (std::ostream &out, const Point &p) {
+        return out << "(" << p.row << ", " <<  p.col << ")";
+    }
+};
+
+
+int lab[labSize][labSize] = {0, 1, 0, 0, 0,
+                             0, 1, 0, 0, 0,
+                             0, 0, 0, 0, 0,
+                             0, 1, 1, 1, 0,
+                             0, 0, 0, 1, 0};
+const int LAB_EMPTY = 0;
+const int LAB_WALL = 1;
+
+bool hasRoute(const Point &start, const Point& end, int lab[][labSize]) {
+    std::stack<Point> route;
+    route.push(start);
+    lab[start.row][start.col] = LAB_WALL;
+
+    const Point directions[] = {
+        Point(0, 1),            // down
+        Point(1, 0),            // right
+        Point(0, -1),           // up
+        Point(-1, 0)            // left
+    };
+    
+    while(!route.empty()) {
+        const Point current = route.top();
+        if(current == end) {
+            break;
+        }
+        route.pop();
+
+        for(std::size_t i = 0; i < 4; ++ i) {
+            const Point next = current + directions[i];
+            if(next.checkBounds() && lab[next.row][next.col] == LAB_EMPTY) {
+                lab[next.row][next.col] = LAB_WALL;
+                route.push(next);
+            }
+        }
+        
+    }
+
+    if(route.empty()) return false;
+    else {
+        std::stack<Point> reversed;
+        while(!route.empty()) {
+            reversed.push(route.top());
+            route.pop();
+        }
+        while(!reversed.empty()) {
+            std::cout << reversed.top() << std::endl;
+            reversed.pop();
+        }
+    }
+    return true;
+}
+
+
+int main() {
+
+    // std::cout << polishNotationConvert("AB+CD-*XY-*") << "\n";
+
+    std::cout << hasRoute({0, 0}, {0,4}, lab) << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+#include <string>
+#include <stack>
+
+bool isOperator(char c) {
+    switch (c) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        return true;
+    default:
+        return false;
+    }
+}
+
+std::string postfixToPrefix(const std::string& expression) {
+    std::stack<std::string> stack;
+
+    for (std::size_t i = 0; i < expression.size(); ++i) {
+        if (isOperator(expression[i])) {
+            assert(stack.size() >= 2);
+            std::string op1 = stack.top();
+            stack.pop();
+            std::string op2 = stack.top();
+            stack.pop();
+
+            std::string temp = expression[i] + op2 + op1;
+            stack.push(temp);
+        } else {
+            stack.push(std::string(1, expression[i]));
+        }
+    }
+    std::string ans = "";
+    while(stack.empty() == false) {
+        ans += stack.top();
+        stack.pop();
+    }
+    return ans;
+}
+
+int main() {
+    std::string expression = "AB+CD-*XY-*"; // "AB+CD-*AB+CD-*";
+
+    std::string prefix = postfixToPrefix(expression);
+    // assert(prefix == "*+AB-CD*+AB-CD");
+    std::cout << prefix << "\n";
+}
+#endif
---
+++ Week04Extra/readme.md
@@ -0,0 +1,18 @@
+## Стек
+## Постфиксен към префиксен запис
+
+https://www.geeksforgeeks.org/postfix-prefix-conversion/
+
+Input :  Postfix : AB+CD-*
+
+Output : Prefix :  *+AB-CD
+
+Explanation : Postfix to Infix : (A+B) * (C-D)
+              Infix to Prefix :  *+AB-CD
+
+Input :  Postfix : ABC/-AK/L-*
+
+Output : Prefix :  *-A/BC-/AKL
+
+Explanation : Postfix to Infix : ((A-(B/C))*((A/K)-L))
+              Infix to Prefix :  *-A/BC-/AKL 
