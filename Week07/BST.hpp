b29290c6bbd245a6fcd56993bcb8296724e918d5
Damian Boytchev <damian.boytchev@gmail.com>
2023-12-01 19:53:55 +0200

    Add iterator for BST.

---
+++ Week07/BST.hpp
@@ -0,0 +1,181 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <stack>
+
+template<typename T>
+class BST {
+	struct Node;
+public:
+
+	class Iterator {
+	public:
+		Iterator(BST* bst, Node* node) :bst(bst) {
+			this->pushLeftChildren(node);
+		}
+		Iterator& operator++() {
+			Node* current = this->nodes.top();
+			this->nodes.pop();
+			this->pushLeftChildren(current->right);
+			return *this;
+		}
+
+		bool operator!=(const Iterator& other) const {
+			if (this->bst != other.bst) {
+				return true;
+			}
+			/*if (this->nodes.empty()) {
+				return !(other.nodes.empty());
+			}
+			if (other.nodes.empty()) {
+				return true;
+			}
+			return this->nodes.top() != other.nodes.top();
+			
+			// OR
+			*/
+			return this->topSafe() != other.topSafe();
+		}
+
+		T& operator*() {
+			return this->nodes.top()->data;
+		}
+
+	private:
+		void pushLeftChildren(Node* node) {
+			while (node != nullptr) {
+				this->nodes.push(node);
+				node = node->left;
+			}
+		}
+
+		Node* topSafe() const {
+			if (this->nodes.empty()) {
+				return nullptr;
+			}
+			return this->nodes.top();
+		}
+	private:
+		std::stack<Node*> nodes;
+		BST* bst;
+	};
+public:
+	BST():root(nullptr) {}
+
+	void insert(const T& newData) {
+		this->insertHelper(this->root, newData);
+	}
+
+	void print() const {
+		this->printHelper(this->root);
+	}
+
+	std::size_t height() const {
+		return this->heightHelper(this->root);
+	}
+
+	void balance() {
+		std::vector<Node*> sortedNodes;
+		this->getSortedNodes(this->root, sortedNodes);
+		this->root = nullptr;
+
+		for(Node* node : sortedNodes) {
+			std::cout << "Node: " << node->data << "\n";
+		}
+		std::cout << "------------\n";
+		this->insertBalanced(this->root, sortedNodes, 0, sortedNodes.size() - 1);
+	}
+
+	Iterator begin() {
+		return Iterator(this, this->root);
+	}
+
+	Iterator end() {
+		return Iterator(this, nullptr);
+	}
+private:
+	struct Node {
+		T data;
+		Node* left;
+		Node* right;
+		
+		Node(const T& data): data(data), left(nullptr), right(nullptr) {}
+
+		Node(const T& data, Node* left, Node* right): data(data), left(left), right(right) {}
+	};
+private:
+	void insertHelper(Node*& node, const T& newData) {
+		if (node == nullptr) {
+			node = new Node(newData);
+			return;
+		}
+		if (newData < node->data) {
+			this->insertHelper(node->left, newData);
+		} else {
+			this->insertHelper(node->right, newData);
+		}
+	}
+	
+	void insertHelperIterative(const T& newData) {
+		Node** insertHere = &(this->root);
+
+		while((*insertHere) != nullptr) {
+			if(newData < (*insertHere)->data) {
+				insertHere = &((*insertHere)->left);
+			} else {
+				insertHere = &((*insertHere)->right);
+			}
+		}
+		(*insertHere) = new Node(newData);
+
+		// SAME AS THIS:
+		// while(true) {
+		// 	if((*insertHere) == nullptr) {
+		// 		(*insertHere) = new Node(newData);
+		// 		return;
+		// 	} else {
+		// 		if(newData < (*insertHere)->data) {
+		// 			insertHere = &((*insertHere)->left);
+		// 		} else {
+		// 			insertHere = &((*insertHere)->right);
+		// 		}
+		// 	}
+		// }
+	}
+	
+	void printHelper(Node * const node) const {
+		if (node == nullptr) { return; }
+		printHelper(node->left);
+		std::cout << node->data << ", ";
+		printHelper(node->right);
+	}
+
+	std::size_t heightHelper(Node* node) const {
+		if (node == nullptr) { return 0; }
+		return 1 + std::max(this->heightHelper(node->left), this->heightHelper(node->right));
+	}
+
+	void getSortedNodes(Node* node, std::vector<Node*>& sortedNodes) {
+		if(node == nullptr) { return; }
+		this->getSortedNodes(node->left, sortedNodes);
+		node->left = nullptr;
+		sortedNodes.push_back(node);
+		this->getSortedNodes(node->right, sortedNodes);
+		node->right = nullptr;
+	}
+
+	void insertBalanced(Node*& node, std::vector<Node*>& sortedNodes, std::size_t left, std::size_t right) {
+		std::cout << "[balanced]: node:" << (node ? node->data : 0) << " left: " << left << " right: " << right << "\n";
+		if (right >= sortedNodes.size() || right < left) { return; }
+		
+		std::size_t mid = left + (right - left) / 2; // same as (left + right) / 2, but protects from overflow
+		std::cout << "Inserting " << sortedNodes[mid]->data << "\n";
+		node = sortedNodes[mid];
+		if (mid > 0) {
+			insertBalanced(node->left, sortedNodes, left, mid - 1);
+		}
+		insertBalanced(node->right, sortedNodes, mid + 1, right);
+	}
+private:
+	Node* root;
+};
---
+++ Week07/main.cpp
@@ -0,0 +1,37 @@
+#include "BST.hpp"
+
+int main() {
+	BST<int> bst;
+	bst.insert(20);
+	bst.insert(10);
+
+	bst.insert(50);
+	bst.insert(69);
+	bst.insert(420);
+
+	bst.print();
+	std::cout << std::endl;
+	if(bst.height() != 4) {
+		std::cout <<"Height function does not work\n";
+	}
+	bst.balance();
+	if(bst.height() != 3) {
+		std::cout <<"Balance does not work\n";
+	}
+
+
+	BST<int>::Iterator it  = bst.begin();
+	BST<int>::Iterator end  = bst.end();
+
+	for(; it != end; ++it) {
+		std::cout << *it << ", ";
+	}
+	std::cout << "\n";
+
+	
+
+
+
+
+	bst.print();
+}
